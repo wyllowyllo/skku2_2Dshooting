db13a99b6ab7489b6a14485edbff61553ac6ad1f
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2025-11-11 13:41:51 +0900

    feat : 배경오브젝트 시차 스크롤링 적용

--- Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour
 {
+    private Material _backgroundMaterial;
+    [SerializeField]private float _scrollSpeed=1.0f;
+
+    private void Start()
+    {
+        _backgroundMaterial = GetComponent<Renderer>().material;
+    }
+
     private void Update()
     {
-        throw new NotImplementedException();
+        //방향을 구한다
+        Vector2 direction = Vector2.up;
+        
+        //움직인다
+        _backgroundMaterial.mainTextureOffset += direction*_scrollSpeed*Time.deltaTime;
     }
 }
