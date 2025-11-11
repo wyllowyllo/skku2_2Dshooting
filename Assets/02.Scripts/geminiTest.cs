af9ee14e9a0168b01880a5ba5ee5d0902791b097
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2025-11-11 10:13:58 +0900

    GeminiTest

---
+++ Assets/02.Scripts/geminiTest.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class geminiTest : MonoBehaviour
+{
+    private int speed = 3;
+    void Update()
+    {
+        transform.position += movePosition();
+        Debug.Log("dfdaf" + speed);
+    }
+    private Vector3 movePosition()
+    {
+        return Vector3.up * Time.deltaTime * speed;
+    }
+}
