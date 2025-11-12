d56d2b7a1133d275b3b1c8069f610146bd7adbcc
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2025-11-12 11:19:25 +0900

    test : {이름, 점수} 저장&로드 기능

--- Assets/02.Scripts/Manager/ScoreManager.cs
+++ Assets/02.Scripts/Manager/ScoreManager.cs
@@ -17,8 +17,16 @@ public class ScoreManager : MonoBehaviour
 
     private void Start()
     {
+        TestLoad();
         Refresh();
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            TestSave();
+        }
     }
 
     public void AddScore(in int score)
@@ -33,5 +41,32 @@ public class ScoreManager : MonoBehaviour
     private void Refresh()
     {
         _currentScoreTextUI.text = $"현재 점수 : {_currentScore} 점";
+    }
+
+    private void TestSave()
+    {
+        // 유니티에서는 값을 지정할 때 'PlayerPrefs' 모듈을 씁니다.
+        // 저장 가능한 자료형은 : int, float , string
+        // 저장을 할 때는 저장할 이름(key)와 값(value) 이 두 형태로 저장을 한다.
+        
+        // 저장 : set
+        // 로드 : get
+        
+        
+        PlayerPrefs.SetInt("score", _currentScore);
+        PlayerPrefs.SetString("name", "전민관");
+        Debug.Log("저장되었습니다.");
+    }
+
+    private void TestLoad()
+    {
+        if (PlayerPrefs.HasKey("name"))
+        {
+            int score = PlayerPrefs.GetInt("score");
+            string name = PlayerPrefs.GetString("name", "전민관"); // default 인자
+        
+            Debug.Log($"{name} : {score}");
+        }
+        
     }
 }
