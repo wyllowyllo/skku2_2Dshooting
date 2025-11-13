48a8495b079403a61161f27e2e43d8f6957c6ad5
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2025-11-13 09:25:44 +0900

    feat : 펫 프리펩 생성

---
+++ Assets/02.Scripts/Pet/Pet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class Pet : PetBase
+{
+    private float _timer = 0f;
+    private Vector2 _targetPosition = Vector2.zero;
+    private Vector2 _lastTargetPosition = Vector2.zero;
+    
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= shotInterval)
+        {
+            Fire();
+            _timer = 0f;
+        }
+
+        LookatTarget();
+        FollowTarget();
+    }
+
+    private void LookatTarget()
+    {
+        
+        if (Vector2.Distance(target.position, _lastTargetPosition) > 0.001f)
+        {
+            _targetPosRecords.Enqueue(target.position);
+        }
+        
+        
+        if (_targetPosRecords.Count > followDelay)
+        {
+            _targetPosition = _targetPosRecords.Dequeue();
+        }
+    }
+    protected override void FollowTarget()
+    {
+        if (target == null) return;
+        
+        transform.position = _targetPosition;
+    }
+
+    protected override void Fire()
+    {
+        if (bulletPrefab == null) return;
+        
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/02.Scripts/Pet/PetBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PetBase : MonoBehaviour
+{
+    [Header("총알 프리펩")]
+    [SerializeField] protected GameObject bulletPrefab;
+    
+    [Header("공격, 팔로우 딜레이")]
+    [SerializeField] protected float shotInterval=2f;
+    [SerializeField] protected int followDelay=7;
+    
+    [Header("팔로우 타겟")]
+    protected Transform target;
+    
+    protected Queue<Vector2> _targetPosRecords;
+    
+    protected abstract void FollowTarget();
+    protected abstract void Fire();
+}
